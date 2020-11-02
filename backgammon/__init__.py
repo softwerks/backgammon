8a3ee29d7b6444f8517b3fed3b05eb4917848265
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2020-11-02 13:54:21 -0800

    Added starting position ID.

--- backgammon/__init__.py
+++ backgammon/__init__.py
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+STARTING_POSITION_ID = "4HPwATDgc/ABMA"
+
+from backgammon import board
 from backgammon import position
