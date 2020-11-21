b3a6b0cd079c507906b5c05d7e4a61ecf09fff01
Sam Beirne <22671090+sambeirne@users.noreply.github.com>
2020-11-21 15:45:29 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.rst", encoding="utf-8") as f:
 
 setup(
     name="backgammon",
-    version="0.1.0",
+    version="0.1.1",
     author="Softwerks",
     author_email="info@softwerks.com",
     description="Backgammon engine for the Backgammon Network.",
